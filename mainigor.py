caccb7c32ffed47dcabbb60598511494365ed040
Igor16 <75946939+Igor16@users.noreply.github.com>
2023-01-30 20:34:10 +0100

    Update mainigor.py

--- mainigor.py
+++ mainigor.py
@@ -8,34 +8,38 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import AllChem
 import operacje_chemiczne
-import math
 from tkinter import filedialog
 
 
 mode="dark" #zmienna przechowująca tryb aplikacji
 base_color="dark-blue"
+czy_podswietlaj=True
 class przycisk_dane():                #klasa, która tworzy przycisk ctk z zapamiętaniem danych. Używam do zapisu danych z fora w przycisku (rozwiązuje errora)
     def __init__(self,okno,zwiazek,lanc):
         self.przycisk = CTkButton(okno,text="Generuj",width=100,command=lambda:rysuj_interface(okno,zwiazek,lanc))
     def gridbutton(self,x,y):
         self.przycisk.grid(row=x, column=y)
-
 class przycisk_dziwne_dane():                #ten sam przycisk co na gorze tylko z inna funkcja
     def __init__(self,okno,zwiazek,lanc):
         self.przycisk = CTkButton(okno,text="Generuj_dziwnie",width=100,command=lambda:rysuj_dziwnie_interface(okno,zwiazek,lanc))
     def gridbutton(self,x,y):
         self.przycisk.grid(row=x, column=y)
-
 def interfejs(okno): #funkcja obsługująca główny interfejs programu
     for widget in okno.winfo_children():
         widget.destroy()
     seq=Seq("AUGGAACGCGAACCCUAC")
     translated=seq.translate()
-    mol = Chem.MolFromSmiles(operacje_chemiczne.wzor_lancucha_aminokwasow(translated))
-
-
-    img1 = CTkImage(light_image=Draw.MolToImage(mol, size=(500, 500)),dark_image=Draw.MolToImage(mol, size=(500, 500)),size=(500,500))
-
+    wzor, ostatni = operacje_chemiczne.wzor_lancucha_aminokwasow(translated)
+    mol = Chem.MolFromSmiles(wzor)
+    hit_bonds = []
+    for x in range(ostatni):
+        hit_bonds.append(x)
+    atomy = (0,ostatni)
+    if(czy_podswietlaj):
+        obrazStartowy=Draw.MolToImage(mol, highlightBonds=hit_bonds,highlightAtoms=atomy,highlightColor=((0,1,0)), size=(500, 500))
+    else:
+        obrazStartowy = Draw.MolToImage(mol,size=(500, 500))
+    img1 = CTkImage(light_image=obrazStartowy,dark_image=obrazStartowy,size=(500,500))
     napis=CTkLabel(okno, text="Wybierz Opcje")
     plikButton = CTkButton(okno, text="Dane z Pliku", command=lambda:wczytaj_z_pliku(okno))
     recznieButton = CTkButton(okno, text="Dane Ręcznie", command=lambda:wczytaj_recznie(okno,lancpoczatkowy="AUGUAA"))
@@ -76,7 +80,7 @@ def wczytaj_recznie(okno,lancpoczatkowy=""): #funkcja wczytująca dane z "palca"
     wejscie = CTkEntry(okno, width=398)
     wejscie.insert(0, lancpoczatkowy)
     szukaj_interface(okno,wejscie)
-    opis=CTkLabel(okno, text="Wpisz kod nici Rna ",width=100)
+    opis=CTkLabel(okno, text="Wpisz kod nici Rna",width=100)
     szukajButton = CTkButton(okno, text="Szukaj Białek", width=140,command=lambda:szukaj_interface(okno,wejscie))
     wrocButton = CTkButton(okno, text="Wroc", width=140,command=lambda:interfejs(okno))
 
@@ -84,9 +88,6 @@ def wczytaj_recznie(okno,lancpoczatkowy=""): #funkcja wczytująca dane z "palca"
     wejscie.grid(row=0,column=1, columnspan=4 )
     szukajButton.grid(row=0,column=5)
     wrocButton.grid(row=1,column=5)
-
-
-
 def szukaj_interface(okno,wejscie): #funkcja wypisująca wszystkie łańcuchy w interfejsie
     lanc=Seq(wejscie.get())
 
@@ -148,12 +149,20 @@ def rysuj_dziwnie_interface(okno,lanc_Kodonow,lancpowrotny):
 def rysuj_interface(okno,lanc_Kodonow,lancpowrotny):
     for widget in okno.winfo_children(): #czycimy okno
         widget.destroy()
-    Smiles=operacje_chemiczne.wzor_lancucha_aminokwasow(lanc_Kodonow)
+    Smiles, ostatni = operacje_chemiczne.wzor_lancucha_aminokwasow(lanc_Kodonow)
     mol = Chem.MolFromSmiles(Smiles)
-    wzor_strukturalny = ImageTk.PhotoImage(Draw.MolToImage(mol, size=(500, 500), kekulize=True, wedgeBonds=True, fitImage=True))
+    hit_bonds = []
+    for x in range(ostatni):
+        hit_bonds.append(x)
+    atomy = (0, ostatni)
+    if(czy_podswietlaj):
+        obraz = Draw.MolToImage(mol, highlightBonds=hit_bonds, highlightAtoms=atomy, size=(500, 500))
+    else:
+        obraz = Draw.MolToImage(mol, size=(500, 500))
+    wzor_strukturalny = CTkImage(light_image=obraz, dark_image=obraz, size=(500, 500))
     WrocButton = CTkButton(okno,text="wroc",command=lambda:wczytaj_recznie(okno,lancpowrotny))
     WykresyButton = CTkButton(okno,text="wykresy",command=lambda:wykresy(okno,lanc_Kodonow,lancpowrotny))
-    obraz=CTkLabel(okno,text="", image=wzor_strukturalny)
+    obraz=CTkLabel(okno, image=wzor_strukturalny, text="")
     obraz.grid(row=0,column=0,rowspan=6)
     WrocButton.grid(row=0,column=1)
     WykresyButton.grid(row=1,column=1)
@@ -172,19 +181,37 @@ def interfaceOpcje(okno): #funkcja wczytująca interface opcji
     napisMode=CTkLabel(okno,text="zmien kolor tla:")
     napisKolor = CTkLabel(okno, text="zmien kolor przycisków:")
     napisWroc = CTkLabel(okno, text="wróc do menu:")
+    napisPodswietlaj=CTkLabel(okno, text="Podświetlać główny łańcuch:")
     przyciskMode= CTkButton(okno,text=mode,command=lambda:zmien_tryb(okno))
     przyciskKolor = CTkButton(okno,text=base_color,command=lambda:zmien_kolor(okno))
-    przyciskWroc= CTkButton(okno,text="Wróc",command=lambda:zapis(okno),border_color=okno._fg_color)
+    przyciskWroc= CTkButton(okno,text="Wróc",command=lambda:zapis(okno))
+    if(czy_podswietlaj==True):
+        przyciskPodswietlaj = CTkButton(okno, text="Tak", command=lambda: podswietlenie(okno))
+    else:
+        przyciskPodswietlaj = CTkButton(okno, text="Nie", command=lambda: podswietlenie(okno))
     przyciskMode.grid(row=1,column=0)
     przyciskKolor.grid(row=3,column=0)
-    przyciskWroc.grid(row=5,column=0)
+    przyciskPodswietlaj.grid(row=5,column=0)
+    przyciskWroc.grid(row=7, column=0)
     napisMode.grid(row=0, column=0)
     napisKolor.grid(row=2, column=0)
-    napisWroc.grid(row=4, column=0)
+    napisPodswietlaj.grid(row=4,column=0)
+    napisWroc.grid(row=6, column=0)
+def podswietlenie(okno):
+    global czy_podswietlaj
+    if (czy_podswietlaj == True):
+        czy_podswietlaj=False
+        interfaceOpcje(okno)  # ta funkcja aktualizuje napisy na przyciskach
+        return 0
+    if (czy_podswietlaj == False):
+        czy_podswietlaj = True
+        interfaceOpcje(okno)  # ta funkcja aktualizuje napisy na przyciskach
+        return 0
 def zapis(okno):
     plik = open("opcje.txt", 'w')
     plik.write(mode+'\n')
     plik.write(base_color+'\n')
+    plik.write(str(czy_podswietlaj) + '\n')
     plik.close()
     interfejs(okno)
 def zmien_tryb(okno):
@@ -230,6 +257,10 @@ if __name__ == '__main__':
     plik.close()
     mode=opcje[0].rstrip()
     base_color = opcje[1].rstrip()
+    if(opcje[2]=="True\n"):
+        czy_podswietlaj=True
+    else:
+        czy_podswietlaj=False
     set_appearance_mode(mode)
     set_default_color_theme(base_color)
     interfejs(okno)
