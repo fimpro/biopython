5184bc3eb6cada21a5d820e917bc848236504758
Shadov15 <125920539+Shadov15@users.noreply.github.com>
2023-02-23 21:55:28 +0100

    Add files via upload

--- operacje_chemiczne.py
+++ operacje_chemiczne.py
@@ -1,6 +1,3 @@
-from rdkit import Chem
-import math
-from Bio.Seq import Seq
 def wzor_lancucha_aminokwasow(x):
     Smiles={
         "S":"N[C@H](C(O)=O)CO",
@@ -60,47 +57,144 @@ def wzor_lancucha_aminokwasow(x):
         if (x=='N' or x=='C'):
             dlugosc+=1
     return slowo, dlugosc
-def translacjaBezBugow(lanc):
-    dl=len(lanc)
-    lanc1=lanc[:math.floor(dl/3)*3]#łancuch 1 to łancuch pełnych trójek aminokwasów, zaczynając od 0 aminokwasu
-    if(dl%3==0):
-        lanc2 = lanc[1:math.floor(dl / 3) * 3-2]
-    else:
-        lanc2=lanc[1:math.floor(dl/3)*3+1]
-    if(dl%3==2):
-        lanc3 = lanc[2:math.floor(dl / 3) * 3 + 2]
-    else:
-        lanc3 = lanc[2:math.floor(dl / 3) * 3 - 1]
-    tl1=Seq(lanc1)
-    tl2=Seq(lanc2)
-    tl3=Seq(lanc3)
-    return (tl1.translate()),tl2.translate(),tl3.translate()
+
 def rozklad_na_bialka(lanc): #funkcja przyjmująca łańcuch kodonów, a zwracająca kandydatów na białka w tablicy
     bialka=[]
 
     j=0
     czy_bialko=False
-    dl=len(lanc)
     for i in lanc: #for który każdy kodon podłacza do danego białka
-        if (i == "*" and czy_bialko): #kodony stop oznaczone są w biopythonie *
+        if (i == "*"): #kodony stop oznaczone są w biopythonie *
             czy_bialko = False
             j += 1
-        if czy_bialko:
+        if (czy_bialko):
             bialka[j] = bialka[j] + i
-        elif(i=="M"):
+        if(i=="M"):
             bialka.append("M")
             czy_bialko=True
     return bialka
-def indeks_hydrofobowy(lanc_Kodonow):  # funkcja ze słownikiem zwraca gotowe tablice do wykorzystwania w wykresie
-    kd = {'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5, 'C': 2.5,
-          'Q': -3.5, 'E': -3.5, 'G': -0.4, 'H': -3.2, 'I': 4.5,
-          'L': 3.8, 'K': -3.9, 'M': 1.9, 'F': 2.8, 'P': -1.6,
-          'S': -0.8, 'T': -0.7, 'W': -0.9, 'Y': -1.3, 'V': 4.2}
+
+def indeks_hydrofobowy(lanc):  # funkcja ze słownikiem -> tablica do wykresu
+    kd = {
+            'A': 1.8,
+            'R': -4.5,
+            'N': -3.5,
+            'D': -3.5,
+            'C': 2.5,
+            'Q': -3.5,
+            'E': -3.5,
+            'G': -0.4,
+            'H': -3.2,
+            'I': 4.5,
+            'L': 3.8,
+            'K': -3.9,
+            'M': 1.9,
+            'F': 2.8,
+            'P': -1.6,
+            'S': -0.8,
+            'T': -0.7,
+            'W': -0.9,
+            'Y': -1.3,
+            'V': 4.2
+        }
     dane_y = []
-    for x in lanc_Kodonow:
+    for x in lanc:
         dane_y.append(kd[x])
     return dane_y
-def listaAtomow(wzor):
-    mol=Chem.MolFromSmiles(wzor)
-    for atom in mol.GetAtoms():
-        print(atom.GetAtomicNum())
+
+def polarnosc(lanc): #słownik z polarnościa -> tablica do wykresu
+    pol = {
+        'A': 0,
+        'R': 52.0,
+        'N': 3.38,
+        'D': 40.7,
+        'C': 1.5,
+        'Q': 3.5,
+        'E': 50,
+        'G': 0,
+        'H': 51.6,
+        'I': 0.1,
+        'L': 0.4,
+        'K': 49.5,
+        'M': 1.4,
+        'F': 0.3,
+        'P': 1.6,
+        'S': 1.7,
+        'T': 1.7,
+        'W': 2.1,
+        'Y': 1.6,
+        'V': 0.1
+    }
+    dane_y = []
+    for x in lanc:
+        dane_y.append(pol[x])
+    return dane_y
+
+kd = {
+            'A': 1.8,
+            'R': -4.5,
+            'N': -3.5,
+            'D': -3.5,
+            'C': 2.5,
+            'Q': -3.5,
+            'E': -3.5,
+            'G': -0.4,
+            'H': -3.2,
+            'I': 4.5,
+            'L': 3.8,
+            'K': -3.9,
+            'M': 1.9,
+            'F': 2.8,
+            'P': -1.6,
+            'S': -0.8,
+            'T': -0.7,
+            'W': -0.9,
+            'Y': -1.3,
+            'V': 4.2
+    }
+
+gra = {
+            'A': 8.1,
+            'R': 10.5,
+            'N': 11.6,
+            'D': 13.0,
+            'C': 5.5,
+            'Q': 10.5,
+            'E': 12.3,
+            'G': 9.0,
+            'H': 10.4,
+            'I': 5.2,
+            'L': 4.9,
+            'K': 11.3,
+            'M': 5.7,
+            'F': 5.2,
+            'P': 8.0,
+            'S': 9.2,
+            'T': 8.6,
+            'W': 5.4,
+            'Y': 6.2,
+            'V': 5.9
+}
+
+listaAtomow = {
+    'A',
+    'R',
+    'N',
+    'D',
+    'C',
+    'Q',
+    'E',
+    'G',
+    'H',
+    'I',
+    'L',
+    'K',
+    'M',
+    'F',
+    'P',
+    'S',
+    'T',
+    'W',
+    'Y',
+    'V',
+}
