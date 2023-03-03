77740ec6706691cad4c85281e3a50e136772cc88
Manitou1337 <90336065+Manitou1337@users.noreply.github.com>
2023-03-03 18:59:26 +0100

    Update wykresy.py

--- wykresy.py
+++ wykresy.py
@@ -15,8 +15,8 @@ MolWt = lambda *x,**y:_rdMolDescriptors._CalcMolWt(*x,**y)
 
 def dane_wykres(lanc, suwak):
     dane = []
-    window = int(suwak.get())
-    analizuj = ProteinAnalysis(lanc)  # nie ma za uj pojecia co to sa te skale ale brzmi madrze
+    window = int(suwak.get()) #suwak
+    analizuj = ProteinAnalysis(lanc)  # analiza proteiny(białka)
     dane.append(analizuj.protein_scale(ProtParamData.kd, window))  # indeks hydrofobowy
     dane.append(analizuj.protein_scale(ProtParamData.em, window))  # surface accessibility
     dane.append(analizuj.protein_scale(ProtParamData.Flex, window))  # Normalized flexibility parameters
@@ -25,7 +25,7 @@ def dane_wykres(lanc, suwak):
     dane.append(len(dane[0]))  # pomaga stworzyc oś X w wykresie
     return dane
 
-def checkbox_event1(okno,lanc_Kodonow):
+def checkbox_event1(okno,lanc_Kodonow): #funkcja działająca po kliknięciu checkboxa
     global c1
     if(c1==True):
         c1=False
@@ -65,33 +65,33 @@ def checkbox_event5(okno,lanc_Kodonow):
         c5 = True
     rysowanie_wykresu(okno,lanc_Kodonow)
 
-def rysowanie_wykresu(okno, lanc_Kodonow):
+def rysowanie_wykresu(okno, lanc_Kodonow): #funkcja rysująca wykres ze zmiennymi danymi
     global c1, c2, c3, c4, c5
     dane_do_wykresu = dane_wykres(lanc_Kodonow, suwak)
-    wartości_hydrofobia = (dane_do_wykresu[0])
-    wartości_dostępność = (dane_do_wykresu[1])
+    wartości_hydrofobia = (dane_do_wykresu[0]) 
+    wartości_dostępność = (dane_do_wykresu[1]) #załadowanie danych o białku
     wartości_parametry = (dane_do_wykresu[2])
     wartości_skala = (dane_do_wykresu[3])
     wartości_niestabilność = (dane_do_wykresu[4])
-    dlugosc = len(wartości_hydrofobia)
+    dlugosc = len(wartości_hydrofobia) #funkcja licząca długość, którą będzie miał wykres
     plt.rcParams.update({'font.size': 18})
     aminokwas = []
     for x in range(dlugosc):
-        aminokwas.append(x+1)
+        aminokwas.append(x+1) #każdej sekwencji przypisujemy numer jednostkowy
     dane_hydrofobia = {'Aminokwasy': aminokwas, 'Indeks hydrofobowy': wartości_hydrofobia}
-    dane_dostępność = {'Aminokwasy': aminokwas, 'Dostępność powierzchniowa': wartości_dostępność}
+    dane_dostępność = {'Aminokwasy': aminokwas, 'Dostępność powierzchniowa': wartości_dostępność} #zamiana danych na przystępne do załadowania do wykresu
     dane_parametry = {'Aminokwasy': aminokwas, 'Zogólnione parametry elastyczności': wartości_parametry}
     dane_skala = {'Aminokwasy': aminokwas, 'Skala Janin transferu energii': wartości_skala}
     dane_niestabilność = {'Aminokwasy': aminokwas, 'indeks niestabilności Granthama R.': wartości_niestabilność}
-    Obraz_cech = plt.Figure(figsize=(10, 5), dpi=50)
-    Wypisz_wykres_cech = Obraz_cech.add_subplot(1, 1, 1)
-    Wykres_szczegółowy = FigureCanvasTkAgg(Obraz_cech, okno)
-    Wykres_szczegółowy.get_tk_widget().grid(row=2, column=0,sticky=N+W+E+S)
+    Obraz_cech = plt.Figure(figsize=(10, 5), dpi=50) #stworzenie figury, w której będzie wykres
+    Wypisz_wykres_cech = Obraz_cech.add_subplot(1, 1, 1) #dodanie wykresu do figury
+    Wykres_szczegółowy = FigureCanvasTkAgg(Obraz_cech, okno) #przełożenie wykresu na format TkIntera
+    Wykres_szczegółowy.get_tk_widget().grid(row=2, column=0,sticky=N+W+E+S) #ułożenie wykresu w oknie
 
-    Dane_indeks_hydrofobowy = pd.DataFrame(dane_hydrofobia)
-    Dane_indeks_hydrofobowy = Dane_indeks_hydrofobowy[['Aminokwasy', 'Indeks hydrofobowy']].groupby('Aminokwasy').sum()
+    Dane_indeks_hydrofobowy = pd.DataFrame(dane_hydrofobia) #załadowanie danych
+    Dane_indeks_hydrofobowy = Dane_indeks_hydrofobowy[['Aminokwasy', 'Indeks hydrofobowy']].groupby('Aminokwasy').sum() #pogrupowanie danych
     if (c1):
-        Dane_indeks_hydrofobowy.plot(kind='line', legend=False, ax=Wypisz_wykres_cech, color='#581266')
+        Dane_indeks_hydrofobowy.plot(kind='line', legend=False, ax=Wypisz_wykres_cech, color='#581266') #narysowanie wykresu od danych, jeśli odpowiadający checkbox jest zaznaczony
     Dane_dostępność_powierzchniowa = pd.DataFrame(dane_dostępność)
     Dane_dostępność_powierzchniowa = Dane_dostępność_powierzchniowa[
         ['Aminokwasy', 'Dostępność powierzchniowa']].groupby('Aminokwasy').sum()
@@ -112,33 +112,33 @@ def rysowanie_wykresu(okno, lanc_Kodonow):
         ['Aminokwasy', 'indeks niestabilności Granthama R.']].groupby('Aminokwasy').sum()
     if (c5):
         Dane_indeks_niestabilności.plot(kind='line', legend=False, ax=Wypisz_wykres_cech, color='#d0e831')
-    Wypisz_wykres_cech.xaxis.set_major_locator(MultipleLocator(1))
-    Wypisz_wykres_cech.yaxis.set_minor_locator(MultipleLocator(1))
+    Wypisz_wykres_cech.xaxis.set_major_locator(MultipleLocator(1)) #wypisanie wszystkich indeksów aminokwasów na osi x
+    Wypisz_wykres_cech.yaxis.set_minor_locator(MultipleLocator(1)) #ustawienie pomniejszych 'ticków' na osi y w odległości 1 od siebie
 
-    Wypisz_wykres_cech.set_title('Cechy fizyczno-chemiczne aminokwasów')
+    Wypisz_wykres_cech.set_title('Cechy fizyczno-chemiczne aminokwasów') #tytuł
     
     analizuj = ProteinAnalysis(lanc_Kodonow)  # tablica do analizowania bialek
-    dane_kwasy = analizuj.get_amino_acids_percent()  # ilosc kazdego z kwasow
-    lista = []
+    dane_kwasy = analizuj.get_amino_acids_percent()  # ilosc kazdego z aminokwasow
+    lista = [] #częstotliwość występowania aminokwasów w tabeli
     for i in dane_kwasy:   dane_kwasy[i] = dane_kwasy[i] * 100
     for i in dane_kwasy:
         lista.append(dane_kwasy.get(i))
     dane = {'Aminokwas': ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', ],
             'Częstotliwość występowania': lista}
 
-    Obraz_aminokwasów = plt.Figure(figsize=(10, 4), dpi=50)
-    Wypisz_wykres_aminokwasów = Obraz_aminokwasów.add_subplot(1, 1, 1)
-    Wykres_aminokwasów = FigureCanvasTkAgg(Obraz_aminokwasów, okno)
-    Wykres_aminokwasów.get_tk_widget().grid(row=1, column=0,sticky=N+W+E+S)
-    Dane_aminokwasów = pd.DataFrame(dane)
-    Dane_aminokwasów = Dane_aminokwasów[['Aminokwas', 'Częstotliwość występowania']].groupby('Aminokwas').sum()
-    Dane_aminokwasów.plot(kind='bar', legend=False, ax=Wypisz_wykres_aminokwasów, rot=0)
-    Wypisz_wykres_aminokwasów.set_title('Częstotliwość występowania aminokwasów w białku w procentach')
+    Obraz_aminokwasów = plt.Figure(figsize=(10, 4), dpi=50) #utworzenie figury
+    Wypisz_wykres_aminokwasów = Obraz_aminokwasów.add_subplot(1, 1, 1) #dodanie wykresu do figury
+    Wykres_aminokwasów = FigureCanvasTkAgg(Obraz_aminokwasów, okno) #zamienienie wykresu na format TkIntera
+    Wykres_aminokwasów.get_tk_widget().grid(row=1, column=0,sticky=N+W+E+S) #ułożenie wykresu w oknie
+    Dane_aminokwasów = pd.DataFrame(dane) #dodanie danych
+    Dane_aminokwasów = Dane_aminokwasów[['Aminokwas', 'Częstotliwość występowania']].groupby('Aminokwas').sum() #grupowanie danych
+    Dane_aminokwasów.plot(kind='bar', legend=False, ax=Wypisz_wykres_aminokwasów, rot=0) #wypisanie danych na wykresie
+    Wypisz_wykres_aminokwasów.set_title('Częstotliwość występowania aminokwasów w białku w procentach') #tytuł
 
 
 def wykresy(ramex, wzor, lanc_Kodonow):  # robocza funkcja do wykresów
     global c1,c2,c3,c4,c5, suwak
-    c1 = 1
+    c1 = 1 #zmienna oznaczająca stan checkboxa
     c2 = 1
     c3 = 1
     c4 = 1
@@ -146,7 +146,7 @@ def wykresy(ramex, wzor, lanc_Kodonow):  # robocza funkcja do wykresów
     okno = CTkFrame(ramex)
     okno.grid(row=0, column=0, rowspan=10, columnspan=10, sticky=N + W + S + E)
     size = (okno.winfo_width(), okno.winfo_height())
-    if (size[0] < 650 or size[1] < 500):  # przy pierwszym włączeniu jest bug funkcji winfo, wynik to zawsze (200,200)
+    if (size[0] < 650 or size[1] < 500):  # przy pierwszym włączeniu jest bug funkcji winfo
         size = (650, 500)
     for widget in okno.winfo_children():  # czyscimy okno
         widget.destroy()
@@ -159,9 +159,9 @@ def wykresy(ramex, wzor, lanc_Kodonow):  # robocza funkcja do wykresów
     okno.columnconfigure(0, weight=3)
     okno.columnconfigure(1, weight=1)
 
-    ramka1 = customtkinter.CTkFrame(master=okno)
-    ramka_wykres = customtkinter.CTkFrame(master=okno)
-    napis = CTkLabel(okno, text="Właściwości Białka")
+    ramka1 = customtkinter.CTkFrame(master=okno) #ramka na przyciski
+    ramka_wykres = customtkinter.CTkFrame(master=okno) #ramka na checkboxy
+    napis = CTkLabel(okno, text="Właściwości Białka") #tytuł
     napiss1 = CTkLabel(ramka_wykres, text="Ustal rozmiar okna (długość interwału")
     napiss2 = CTkLabel(ramka_wykres, text="używanego do obliczania profilu wykresu)")
     suwak = CTkSlider(master=ramka_wykres, width=200, from_=2, to=len(lanc_Kodonow)-1, orientation="horizontal")
@@ -174,20 +174,20 @@ def wykresy(ramex, wzor, lanc_Kodonow):  # robocza funkcja do wykresów
     ramka_wykres.grid(row=2,column=1,sticky=N+W+E+S)
     ramka_wykres.columnconfigure(0,weight=1)
     for x in range(15):
-        ramka_wykres.rowconfigure(x, weight=1)
+        ramka_wykres.rowconfigure(x, weight=1) #ustawienie wartości dla wszystich rzędów i kolumn
     ramka1.columnconfigure(0,weight=1)
     for x in range(5):
         ramka1.rowconfigure(x, weight=1)
-    returnButton.grid(row=1, column=0, sticky=E+W)
+    returnButton.grid(row=1, column=0, sticky=E+W) 
     DaneButton.grid(row=3, column=0, sticky=E+W)
 
-    check_var1 = tkinter.IntVar(value=1)
+    check_var1 = tkinter.IntVar(value=1) #zmienna bool w tkinterze
     check_var2 = tkinter.IntVar(value=1)
     check_var3 = tkinter.IntVar(value=1)
     check_var4 = tkinter.IntVar(value=1)
     check_var5 = tkinter.IntVar(value=1)
     checkbox_indeks_hydrofobowy = customtkinter.CTkCheckBox(master=ramka_wykres, text="Indeks hydrofobowy",text_color='#581266', command=lambda: checkbox_event1(okno,lanc_Kodonow),
-                                         variable=check_var1, onvalue=1, offvalue=0)
+                                         variable=check_var1, onvalue=1, offvalue=0) #checkboxy :)
     checkbox_dostępność_powierzchniowa = customtkinter.CTkCheckBox(master=ramka_wykres, text="Dostępność powierzchniowa",text_color='#3fc2d1', command=lambda: checkbox_event2(okno,lanc_Kodonow),
                                          variable=check_var2, onvalue=1, offvalue=0)
     checkbox_zogólnione_parametry_elastyczności = customtkinter.CTkCheckBox(master=ramka_wykres, text="Zogólnione parametry elastyczności",text_color='#057a13', command=lambda: checkbox_event3(okno,lanc_Kodonow),
@@ -225,9 +225,8 @@ def dane_interfejs(ramex, wzor, lanc_Kodonow):
     analizuj = ProteinAnalysis(lanc_Kodonow)
     dw = []
     dw.append(analizuj.secondary_structure_fraction())
-    # zwraca tablice z 3 wartościami, ktore zawieraja %: sheets, helixes, turns cokolwiek by to nie było XD
     dw.append(analizuj.isoelectric_point())
-    dw.append(operacje_chemiczne.pH_bialka(lanc_Kodonow))
+    dw.append(operacje_chemiczne.pH_bialka(lanc_Kodonow)) #obliczanie wszystkich potrzebnych danych
     dw.append(analizuj.instability_index())
     if (analizuj.instability_index() <= 40):
         dw.append("białko stabilne")
@@ -238,7 +237,7 @@ def dane_interfejs(ramex, wzor, lanc_Kodonow):
     linijka_1 = CTkLabel(okno, text="Na strukturę drugorzędową tego białka składa się:")
     linijka_2 = CTkLabel(okno, text=str("- "+str(round(struktura[0] * 100,3)) + " % harmonijek beta (pofałdowanej płaszczyzny)"))
     linijka_3 = CTkLabel(okno, text=str("- "+str(round(struktura[1] * 100,3)) + " % helis alfa (helis pi)"))
-    linijka_4 = CTkLabel(okno, text=str("- "+str(round(struktura[2] * 100,3)) + " % beta zakrętów (pętli omega)"))
+    linijka_4 = CTkLabel(okno, text=str("- "+str(round(struktura[2] * 100,3)) + " % beta zakrętów (pętli omega)")) #wypisanie wszystkich potrzebnych danych
     linijka_5 = CTkLabel(okno, text="Punkt izoelektryczny tego białka wynosi " + str(round(dw[1],3)))
     linijka_6 = CTkLabel(okno, text="W punkcie izoelektrycznym to białko jest " + dw[2])
     linijka_7 = CTkLabel(okno, text="Indeks niestabilności Guruprasada wynosi " + str(round(dw[3],3)))
@@ -248,7 +247,7 @@ def dane_interfejs(ramex, wzor, lanc_Kodonow):
     linijka_2.grid(row=1, column=0, sticky=W+E)
     linijka_3.grid(row=2, column=0, sticky=W+E)
     linijka_4.grid(row=3, column=0, sticky=W+E)
-    linijka_5.grid(row=4, column=0, sticky=W+E)
+    linijka_5.grid(row=4, column=0, sticky=W+E) #ułożenie danych w oknie
     linijka_6.grid(row=5, column=0, sticky=W+E)
     linijka_7.grid(row=6, column=0, sticky=W+E)
     linijka_8.grid(row=7, column=0, sticky=W+E)
